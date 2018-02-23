4643178cc77b3a29a0b54c46b8602c941abf243a
Kevin Li <kevinjcli@hotmail.com>
2018-02-23 04:15:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
+tic_or_tac = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ",]
 
+def display_board 
+  
+  puts "#{tic_or_tac[0]}|#{tic_or_tac[1]}|#{tic_or_tac[2]}"
+  puts "-----------"
+  puts "#{tic_or_tac[3]}|#{tic_or_tac[4]}|#{tic_or_tac[5]}"
+  puts "-----------"
+  puts "#{tic_or_tac[6]}|#{tic_or_tac[7]}|#{tic_or_tac[8]}"
+end
